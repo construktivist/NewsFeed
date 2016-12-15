ec24689d0b89013c67369eb7e7396471789cd7bf
Keegan Kelly <keegankelly1@gmail.com>
2016-12-14 22:28:25 -0600

    Created render path for webpage. Configured server. Added package dependecies.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require("express");
+var path = require('path');
 var bodyParser = require("body-parser");
 var logger = require("morgan");
+var methodOverride = require('method-override');
 var mongoose = require("mongoose");
 var request = require("request");
 var cheerio = require("cheerio");
 var Promise = require("bluebird");
-
-var scrape_controller = require("./models/Scrape.js");
+var scrape_controller = require("./controllers/scrape_controller.js")
+//var Scrape = require("./models/Scrape.js");
 //var Article = require("./models/Article.js");
 
 mongoose.Promise = Promise;
@@ -18,7 +20,6 @@ app.use(methodOverride('_method'))
 
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use(express.static("public"));
